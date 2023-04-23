668b430eb897fa6d30780f56635f9bbba4d5e6a9
Aromakc <suwashkhatry38@gmail.com>
2023-04-23 18:03:50 +0545

    worked on client/addevent

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,9 +12,37 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="nav-left">
         <div className="logo-container">
-          <img src="logo.svg" height="90px" width="90px" className="logo">
-            {/* <svg:use xlink:href="logo.svg#logo"></svg:use> */}
-          </img>
+          {/* <img src="logo.svg" height="90px" width="90px" className="logo"> */}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 147.91 112.89">
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="Layer_1-2" data-name="Layer 1">
+                <text
+                  transform="translate(4.53 45.98) scale(1.01 1)"
+                  font-size="54.09"
+                  font-family="Fenway-Regular, Fenway"
+                  style={{ isolation: 'isolate' }}
+                >
+                  Event
+                </text>
+                <text
+                  transform="matrix(0.97, -0.03, 0.04, 1, 24.96, 84.23)"
+                  font-size="44.04"
+                  font-family="Fenway-Regular, Fenway"
+                  style={{ isolation: 'isolate' }}
+                >
+                  N
+                </text>
+                <text
+                  transform="matrix(0.97, -0.03, 0.04, 1, 64.22, 84.23)"
+                  font-size="54.13"
+                  font-family="Fenway-Regular, Fenway"
+                  style={{ isolation: 'isolate' }}
+                >
+                  exus
+                </text>
+              </g>
+            </g>
+          </svg>
         </div>
         <div className="searchbar-container">
           <Searchbar />
@@ -23,7 +51,10 @@ const Navbar = () => {
       <div className="nav-right">
         <ul className="nav-list">
           <li>
-            <button className="createEventBtn" onClick={() => navigate('/create-event')}>
+            <button
+              className="createEventBtn"
+              onClick={() => navigate('/create-event')}
+            >
               {' '}
               <spam>Create an Event</spam>
             </button>
