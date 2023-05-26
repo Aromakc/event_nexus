9e8ea3fabb87249799eb5c1b20251d6b54a5ec7c
AayushPokharel <official@aayushpokharel.com>
2023-05-26 17:36:00 +0545

    urls for graphql updated.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from graphene_django.views import GraphQLView
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/admin/")),
+    path("", include("events.urls")),
+    # path("", RedirectView.as_view(url="/admin/")),
     path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(graphiql=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
